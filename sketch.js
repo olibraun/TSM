e6c3c8cd12fa9af4737c4de8f10e06aa676032d8
olibraun <oliverbraun89@gmail.com>
2018-03-23 20:28:41 +0100

    refactor: Styling

--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ let cities = [];
 let numberOfCities = 5;
 // Create an index array to be [0..numberOfCities-1]:
 // [...Array(n).keys()] creates the array [0, 1, ..., n-1]
-let indexArray = [...Array(numberOfCities).keys()]
+let indexArray = [...Array(numberOfCities).keys()];
 
 function setup() {
   createCanvas(400, 400);
