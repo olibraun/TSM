99cbbc8ae8966573a5487316b496b5fe87ecf664
olibraun <oliverbraun89@gmail.com>
2018-03-23 20:26:48 +0100

    feat: Function to display a path between the cities.

--- sketch.js
+++ sketch.js
@@ -2,6 +2,22 @@
 
 let cities = [];
 let numberOfCities = 5;
+// Create an index array to be [0..numberOfCities-1]:
+// [...Array(n).keys()] creates the array [0, 1, ..., n-1]
+let indexArray = [...Array(numberOfCities).keys()]
+
+function display(order) {
+  push();
+  stroke(255);
+  strokeWeight(2);
+  noFill();
+  beginShape();
+  for(let i=0; i<numberOfCities; i++) {
+    vertex(cities[i].x, cities[i].y);
+  }
+  endShape();
+  pop();
+}
 
 function setup() {
   createCanvas(400, 400);
@@ -15,6 +31,7 @@ function draw() {
   cities.forEach(city => {
     city.show();
   });
+  display(indexArray);
 }
 
 class City {
@@ -26,7 +43,7 @@ class City {
   show() {
     push();
     stroke(255);
-    strokeWeight(8);
+    strokeWeight(10);
     point(this.x, this.y);
     pop();
   }
