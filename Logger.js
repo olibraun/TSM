bf13692254ff1933434424586eb95d44b31cf882
olibraun <oliverbraun89@gmail.com>
2018-03-25 12:37:14 +0200

    docs: Comments

--- Logger.js
+++ Logger.js
@@ -11,17 +11,24 @@ class Logger {
   }
 
   display() {
+    // Margin offset
     const xoffset = 10;
     const yoffset = 10;
+
+    // Draw background and configure graphics
     diagramGraphics.background(51);
     diagramGraphics.push();
     diagramGraphics.stroke(255);
     diagramGraphics.strokeWeight(1);
     diagramGraphics.noFill();
+
+    // Calculate relevant data
     const l = this.distances.length;
     const spacing = (myGraphicsWidth - 2*xoffset) / l;
     const max = l > 0 ? this.distances[0] : 1;
     const scaling = l > 0 ? (myGraphicsHeight - 2*yoffset)/max : 0;
+
+    // Draw line between the diagram's points
     diagramGraphics.beginShape();
     for(let i=0; i < l; i++) {
       const xpos = xoffset + i*spacing;
@@ -29,6 +36,8 @@ class Logger {
       diagramGraphics.vertex(xpos, ypos);
     }
     diagramGraphics.endShape();
+
+    // Draw points (thicker than the line)
     diagramGraphics.strokeWeight(4);
     for(let i=0; i < l; i++) {
       const xpos = xoffset + i*spacing;
