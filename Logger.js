49f75f46cf36c264b3630947c860a4616e9ddefd
olibraun <oliverbraun89@gmail.com>
2018-03-25 12:36:12 +0200

    feat: Include line between diagram points

--- Logger.js
+++ Logger.js
@@ -16,13 +16,24 @@ class Logger {
     diagramGraphics.background(51);
     diagramGraphics.push();
     diagramGraphics.stroke(255);
-    diagramGraphics.strokeWeight(10);
+    diagramGraphics.strokeWeight(1);
+    diagramGraphics.noFill();
     const l = this.distances.length;
     const spacing = (myGraphicsWidth - 2*xoffset) / l;
     const max = l > 0 ? this.distances[0] : 1;
     const scaling = l > 0 ? (myGraphicsHeight - 2*yoffset)/max : 0;
+    diagramGraphics.beginShape();
     for(let i=0; i < l; i++) {
-      diagramGraphics.point(xoffset + i*spacing, yoffset + myGraphicsHeight - this.distances[i]*scaling);
+      const xpos = xoffset + i*spacing;
+      const ypos = yoffset + myGraphicsHeight - this.distances[i]*scaling;
+      diagramGraphics.vertex(xpos, ypos);
+    }
+    diagramGraphics.endShape();
+    diagramGraphics.strokeWeight(4);
+    for(let i=0; i < l; i++) {
+      const xpos = xoffset + i*spacing;
+      const ypos = yoffset + myGraphicsHeight - this.distances[i]*scaling;
+      diagramGraphics.point(xpos, ypos);
     }
     diagramGraphics.pop();
   }
