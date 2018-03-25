09ad947666257b78a827df3bf1c36e151a4c0923
olibraun <oliverbraun89@gmail.com>
2018-03-25 12:33:28 +0200

    feat: Include x- and y-offset for digram drawing

--- Logger.js
+++ Logger.js
@@ -11,16 +11,18 @@ class Logger {
   }
 
   display() {
+    const xoffset = 10;
+    const yoffset = 10;
     diagramGraphics.background(51);
     diagramGraphics.push();
     diagramGraphics.stroke(255);
     diagramGraphics.strokeWeight(10);
     const l = this.distances.length;
-    const spacing = myGraphicsWidth / l;
+    const spacing = (myGraphicsWidth - 2*xoffset) / l;
     const max = l > 0 ? this.distances[0] : 1;
-    const scaling = l > 0 ? myGraphicsHeight/max : 0;
+    const scaling = l > 0 ? (myGraphicsHeight - 2*yoffset)/max : 0;
     for(let i=0; i < l; i++) {
-      diagramGraphics.point(i*spacing, myGraphicsHeight - this.distances[i]*scaling);
+      diagramGraphics.point(xoffset + i*spacing, yoffset + myGraphicsHeight - this.distances[i]*scaling);
     }
     diagramGraphics.pop();
   }
